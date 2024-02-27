f186053d9c19ce6296cc5dc839d909f261a06443
nazsakib <sakibsnaz@gmail.com>
2024-02-27 13:53:27 +0600

    initial commit

---
+++ delculator.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * Plugin Name: Delculator
+ * Plugin URI: https://sakibsnaz.vercel.app/projects/delculator
+ * Description: Calculate Your Delivery Charge by Using Distance, Weight and Area
+ * Version: 1.0.0
+ * Author: Sakib MD Nazmush
+ * Author URI: https://sakibsnaz.vercel.app
+ * License: GPLv2 or later
+ * Text Domain: delculator
+ */
+
+?>
---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta name="description" content="Calculate Your Delivery Charge by Using Distance, Weight and Area" />
+
+    <!-- style -->
+    <link rel="stylesheet" href="assets/css/style.css" type="text/css" />
+
+    <title>Delculator</title>
+</head>
+
+<body class="bg-slate-900">
+    <header></header>
+    <main>
+        <h1 class="text-white text-4xl">Delculator</h1>
+    </main>
+    <footer></footer>
+</body>
+
+</html>
